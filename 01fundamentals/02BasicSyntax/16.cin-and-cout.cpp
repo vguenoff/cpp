54d0a62f179f233aeee8b64695ab3cf048cc6f70
Vasil Genov <vasil.genov@psinteractive.io>
2023-11-04 12:30:23 +0200

    feat: formatter

--- 01fundamentals/02BasicSyntax/16.cin-and-cout.cpp
+++ 01fundamentals/02BasicSyntax/16.cin-and-cout.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
   int a, b;
   cin >> a >> b;
   b = 5;
+
   cout << a * b + 5 << endl;
 
   return 0;

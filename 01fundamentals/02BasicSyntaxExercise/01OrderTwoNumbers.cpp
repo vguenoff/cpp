8e95a649d54f7bcb689e0eba8053af519295cf06
Vasil Genov <vasil.genov@psinteractive.io>
2023-11-06 09:01:22 +0200

    feat: exercises wip

---
+++ 01fundamentals/02BasicSyntaxExercise/01OrderTwoNumbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// Write a program that:
+// · Read two integers from the console
+// · Print the two numbers in increasing order
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a < b) {
+    cout << a << " " << b << endl;
+  } else {
+    cout << b << " " << a << endl;
+  }
+
+  return 0;
+}
---
+++ 01fundamentals/02BasicSyntaxExercise/02ProductSign.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+// Write a program that shows the sign (+ or -) of the product of three real
+// numbers without calculating it.
+// · Read 3 real numbers from the console (on a single line, separated by
+// spaces)
+// · Print the sign of their product(if the product is 0, print '+')
+
+int main() {
+  double a, b, c;
+  cin >> a >> b >> c;
+
+  // if (a * b * c >= 0) {
+  //   cout << "+" << endl;
+  // } else {
+  //   cout << "-" << endl;
+  // }
+
+  if (a < 0 && b >= 0 && c >= 0) {
+    cout << "-" << endl;
+  } else if (a >= 0 && b < 0 && c >= 0) {
+    cout << "-" << endl;
+  } else if (a >= 0 && b >= 0 && c < 0) {
+    cout << "-" << endl;
+  } else if (a < 0 && b < 0 && c >= 0) {
+    cout << "+" << endl;
+  } else if (a >= 0 && b < 0 && c < 0) {
+    cout << "+" << endl;
+  } else if (a < 0 && b >= 0 && c < 0) {
+    cout << "+" << endl;
+  } else if (c < 0 && b < 0 && a < 0) {
+    cout << "-" << endl;
+  } else {
+    cout << "+" << endl;
+  }
+
+  return 0;
+}
